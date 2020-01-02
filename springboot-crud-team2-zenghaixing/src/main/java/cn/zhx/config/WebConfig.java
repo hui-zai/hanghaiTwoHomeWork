35e8f9e418d02169c9060f5fb33e6937b2c18c29
ivey <ivey@126.com>
2020-01-02 16:23:20 +0800

    作业项目的增删改查完成版提交

---
+++ springboot-crud-team2-zenghaixing/src/main/java/cn/zhx/config/WebConfig.java
@@ -0,0 +1,14 @@
+package cn.zhx.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.testng.annotations.ITestOrConfiguration;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        //注册一个视图控制器：url为："/study"，视图名为："xing"--对应页面：paidaxing/xing.html
+        registry.addViewController("/crud").setViewName("hai");
+    }
+}
