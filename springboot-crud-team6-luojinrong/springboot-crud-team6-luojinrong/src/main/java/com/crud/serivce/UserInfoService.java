dbb0f80b64e9f7dca29ac4cbabc14d38784b6883
Aubrey <2356222374@qq.com>
2020-01-03 17:06:29 +0800

    计科165-27-罗锦荣 版本提交

---
+++ springboot-crud-team6-luojinrong/springboot-crud-team6-luojinrong/src/main/java/com/crud/serivce/UserInfoService.java
@@ -0,0 +1,19 @@
+package com.crud.serivce;
+
+import com.crud.pojo.UserInfo;
+
+import java.util.List;
+
+
+
+public interface UserInfoService {
+    //查询所有用户信息
+    public List<UserInfo> findAll();
+    //新增用户信息
+    public void save(UserInfo userInfo);
+    //修改用户信息
+    public void update(UserInfo userInfo);
+    //删除用户信息
+    public void deleteById(Integer id);
+
+}
