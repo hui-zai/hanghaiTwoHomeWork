e1890d595111fb0c44cd3a59d60c5b7892683883
rari <852288794@qq.com>
2020-01-03 14:53:22 +0800

    team5-谢梓涛-作业项目提交

--- springboot-crud-team5-xiezitao/crud/src/main/java/cn/rari/dao/UserInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package cn.rari.dao;
-
-import cn.rari.pojo.UserInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface UserInfoRepository extends JpaRepository<UserInfo,Integer>, JpaSpecificationExecutor<UserInfo> {
-}
