07972551fcd18e7f5fe655315aa7957616b5f5fa
yyuan <ooo>
2020-01-03 15:15:13 +0800

    钟元元team5最终作业

---
+++ springboot-crud-team5-zhongyuanyuan/src/main/java/com/zyy/pojo/ResponseObject.java
@@ -0,0 +1,15 @@
+package com.zyy.pojo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseObject {
+    private String msg;
+    private String code;
+    private Object object;
+}
