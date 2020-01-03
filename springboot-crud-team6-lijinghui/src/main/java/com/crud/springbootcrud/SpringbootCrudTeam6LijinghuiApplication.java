3d792f38ab2cdb723d315cfbc0c35bb3b4f8cd5d
Michel <1097853056@qq.com>
2020-01-03 10:57:07 +0800

    黎景辉第一次提交项目

---
+++ springboot-crud-team6-lijinghui/src/main/java/com/crud/springbootcrud/SpringbootCrudTeam6LijinghuiApplication.java
@@ -0,0 +1,13 @@
+package com.crud.springbootcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudTeam6LijinghuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCrudTeam6LijinghuiApplication.class, args);
+    }
+
+}
