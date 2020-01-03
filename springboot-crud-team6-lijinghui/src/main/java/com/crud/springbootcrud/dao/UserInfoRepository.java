a8e456b3bdf291e3e784cce6ca966df9655d998e
Michel <1097853056@qq.com>
2020-01-03 10:54:07 +0800

    黎景辉第一次提交项目

---
+++ springboot-crud-team6-lijinghui/src/main/java/com/crud/springbootcrud/dao/UserInfoRepository.java
@@ -0,0 +1,9 @@
+package com.crud.springbootcrud.dao;
+
+
+import com.crud.springbootcrud.pojo.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface UserInfoRepository extends JpaRepository<UserInfo,Integer>, JpaSpecificationExecutor<UserInfo> {
+}
